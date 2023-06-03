642ac8790bf6d59563b9ae4bb7bfa8d5669a3bae
barrotbake <nguyenjacob411@gmail.com>
2023-06-03 00:49:19 -0700

    added front end for regions and pf section

--- src/components/PokeProfile.svelte
+++ src/components/PokeProfile.svelte
@@ -1,11 +1,45 @@
-<div>
-    <h1>
-        Im the profile
-    </h1>
+<div class="profile-box">
+	<img class="profile-pic" src="../mudkip.png" alt="mudkip" />
+	<h1>Mudkip</h1>
+	<div class="exp-container">
+		<h2>EXP</h2>
+		<div class="progress-bar">
+			<div class="progress-bar-inner" />
+		</div>
+	</div>
 </div>
 
-<style>
-    div {
-        border: 1px solid black;
+<style lang="scss">
+    .profile-box {
+        border-right: 5px solid #D5A786;
+        border-bottom: 2.5px solid #D5A786;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .exp-container, .profile-pic, h1, h2 {
+            margin: 15px;
+        }
+    }
+    .progress-bar {
+        width: 100%;
+        height: 20px;
+        background-color: #D9D9D9;
+        margin: 10px;
+    }
+    .progress-bar-inner {
+        width: 50%;
+        height: 100%;
+        background-color: #8FC9FF;
+    }
+    .profile-pic {
+        width: 220px;
+        height: 200px;
+        border: 5px solid #D5A786;
+        border-radius: 100%;
+    }
+    .exp-container {
+        display: flex;
+        flex-direction: column;
+        width: 80%;
     }
 </style>
--- src/components/RegionNav.svelte
+++ src/components/RegionNav.svelte
@@ -1,11 +1,63 @@
-<div>
-    <h1>
-        Im the region nav
-    </h1>
-</div>
-
-<style>
-    div {
-        border: 1px solid black;
+<script lang="ts">
+     export let boardList: Array<String> = [
+        "Kanto",
+        "Johto",
+        "Hoenn",
+     ];
+     let activeRegion: String = "Kanto";
+</script>
+<div class="region-container">
+   <div class="add-region-container">
+       <h1>Regions</h1>
+       <button>+</button>
+   </div>
+   {#each boardList as board}
+    {#if board == activeRegion}
+        <div class="region-list-item">
+            <img src="../activeRegion.svg" alt="">
+            <p style="font-weight: bold; color: #6C3D1B">{board}</p>
+        </div>
+    {:else}
+        <div class="region-list-item">
+            <div class="inactive"></div>
+            <p>{board}</p>
+        </div>
+    {/if}
+    {/each}
+</div>  
+<style lang="scss">
+    .region-container {
+        border-right: 5px solid #D5A786;
+        border-top: 2.5px solid #D5A786;
+        min-height: 100%;
+    }
+    .add-region-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin: 15px;
+        button {
+            font-size: larger;
+            font-weight: bold;
+            border: none;
+        }
+    }
+    .region-list-item {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin: 10px;
+        width: auto;
+        font-weight: 600;
+        img {
+            margin-right: 1.5em;
+        }
+        .inactive {
+            width: 27px;
+            height: 28px;
+            background-color: #AA988A;
+            margin-right: 1.5em;
+        }
     }
 </style>
